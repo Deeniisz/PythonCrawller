9a0e1e2f627bd15b660f929a8aef3e7aaed98f66
Deeniisz <assis.denis98@gmail.com>
2019-04-20 14:13:20 -0300

    Updating 20/04/19

--- PythonCrawller.py
+++ PythonCrawller.py
@@ -2,34 +2,34 @@ import urllib.request
 from bs4 import BeautifulSoup
 import time
 
-url = "https://www.submarino.com.br/categoria/informatica/notebook/f/entrega-prime/processador-intel%20core%20i5?ordenacao=lowerPrice&origem=omega"
+print("Qual Pagina deseja Ler:")
+pagina = input()
+
+url = "https://www.carrefour.com.br/busca?termo=Notebook&isGrid=true&sort=&page={}&foodzipzone=na".format(pagina)
 req = urllib.request.Request(url , headers={'User-Agent': 'Mozilla/5.0'})
 
 webpage = urllib.request.urlopen(req).read()
-time.sleep(10)
+time.sleep(2)
 soup = BeautifulSoup(webpage, "html.parser")
 
-
-
-formatedname = soup.findAll("h1", "TitleUI-sc-1m3ayw0-16 cvOkHP TitleH1-c6mv26-0 hPNDEG")
-formatedprice = soup.findAll("span", "PriceUI-sc-1m3ayw0-10 pYYOk PriceUI-pftkg3-0 hDaJyt TextUI-sc-1hrwx40-0 hbVZKK")
+formatedname = soup.findAll("h3", "prd-name")
+formatedprice = soup.findAll("span", "prd-price-new")
 print(len(formatedprice))
 print(len(formatedname))
 name = []
 price = []
 
 for i in formatedname:
-    line = str(i).replace('<h1 class="TitleUI-sc-1m3ayw0-16 cvOkHP TitleH1-c6mv26-0 hPNDEG" icon="[object Object]">', "").replace("</h1>", "")
+    line = str(i).replace('<h3 class="prd-name">', "").replace("</h3>", "").strip()
     name.append(line)
 
 for i in formatedprice:
-    line = str(i).replace('<span class="PriceUI-sc-1m3ayw0-10 pYYOk PriceUI-pftkg3-0 hDaJyt TextUI-sc-1hrwx40-0 hbVZKK">', "").replace("<!-- -->", "").replace("</span>", "")
+    line = str(i).replace('<span class="prd-price-new">', "").replace("<bdi>", "").replace("</bdi>", "").replace("</span>", "").strip()
     price.append(line)
 
 
 
-"""i = 0
-for info in price:
-    print(name[i] + "-" + price[i])
+i = 0
+for info in name:
+    print(name[i] + " - " + price[i])
     i += 1 
-"""
